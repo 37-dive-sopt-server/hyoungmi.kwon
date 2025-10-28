c2f6ca8e4091816e49f87497b967652f7b6d1803
Hyoungmi Kwon <108400640+hyomee2@users.noreply.github.com>
2025-10-28 16:44:17 +0900

    feat: 2주차 세미나 (#7)

--- src/main/java/org/sopt/controller/MemberController.java
+++ src/main/java/org/sopt/controller/MemberController.java
@@ -2,29 +2,41 @@ package org.sopt.controller;
 
 import org.sopt.domain.Member;
 import org.sopt.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 import java.util.Optional;
 
+@RestController
 public class MemberController {
 
-    private final MemberService memberService;
+    @Autowired
+    private MemberService memberService;
 
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+//    public MemberController(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 
+    @PostMapping("/users")
     public Member createMember(String name, String birthdayStr, String email, String genderStr) {
         return memberService.join(name, birthdayStr, email, genderStr);
     }
 
+    @GetMapping("/users")
     public Optional<Member> findMemberById(Long id) {
         return memberService.findOne(id);
     }
 
+    @GetMapping("/users/all")
     public List<Member> getAllMembers() {
         return memberService.findAllMembers();
     }
 
+    @DeleteMapping("/users")
     public void deleteMemberById(Long id) {
         memberService.delete(id);
     }
