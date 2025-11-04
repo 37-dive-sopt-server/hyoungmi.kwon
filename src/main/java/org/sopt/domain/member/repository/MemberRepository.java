b4ba9285d5c5bfd436773b7b872159bbe3b0156e
hyomee2 <s5763305@naver.com>
2025-11-04 09:51:52 +0900

    feat: 3주차 세미나

--- src/main/java/org/sopt/domain/member/repository/MemberRepository.java
+++ src/main/java/org/sopt/domain/member/repository/MemberRepository.java
@@ -1,19 +1,9 @@
 package org.sopt.domain.member.repository;
 
 import org.sopt.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface MemberRepository {
-
-    Member save(Member member);
-
-    Optional<Member> findById(Long id);
-
-    List<Member> findAll();
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
-
-    void deleteById(Long id);
 }
