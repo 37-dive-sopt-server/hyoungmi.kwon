83ce436d6a4087fbaceb2b0a7e355f2a1b2481f6
hyomee2 <s5763305@naver.com>
2025-11-04 10:35:42 +0900

    refactor: DTO 형식 검증 어노테이션 수정

--- src/main/java/org/sopt/domain/member/dto/request/MemberCreateRequestDTO.java
+++ src/main/java/org/sopt/domain/member/dto/request/MemberCreateRequestDTO.java
@@ -12,11 +12,11 @@ public record MemberCreateRequestDTO(
         @NotBlank(message = "이름은 필수입니다.")
         String name,
 
-        @NotBlank(message = "생년월일은 필수입니다.")
+        @NotNull(message = "생년월일은 필수입니다.")
         @Past(message = "생년월일은 과거 날짜여야 합니다.")
         LocalDate birthdate,
 
-        @NotNull(message = "이메일은 필수입니다.")
+        @NotBlank(message = "이메일은 필수입니다.")
         @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email,
 
